b73e12b8c1a07f537ccfed22d4dfc9456b476ebb
Toma Lupu <>
2021-04-12 10:07:18 +0000

    Fisrt commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let sitesLinks = [];
+
+let addNewListElement = function(event){
+
+  event.preventDefault();
+
+  let lisElementUrl = $("#form-url").val();
+  let lisElementTitle = $("#form-title").val();
+  let lisElementCtaegory = $("#form-category").val();
+
+  let site = {
+
+    url: lisElementUrl,
+    title: lisElementTitle,
+    category: lisElementCtaegory,
+  }
+
+  sitesLinks.push(site);
+  console.log(sitesLinks);
+  createNewLink(sitesLinks);
+}
+
+let createNewLink = function(links){
+
+  links.forEach(link => {
+
+    const aElem = $("<a>").attr("href",link.url).text(link.title);
+
+    const listElement = $("<li>").append(aElem);
+    $("#list").append(listElement);
+  });
+
+  
+}
+
+let init = function(){
+
+  $("form").submit(addNewListElement)
+}
+
+$(document).ready(init);
